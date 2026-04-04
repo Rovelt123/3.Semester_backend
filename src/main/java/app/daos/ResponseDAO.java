669a49c7aa2cff005e1443ef9afb2235bdddf761
Rovelt123 <78000528+Rovelt123@users.noreply.github.com>
2026-04-04 20:03:32 +0200

    Fixed responseDAO

--- src/main/java/app/daos/ResponseDAO.java
+++ src/main/java/app/daos/ResponseDAO.java
@@ -16,21 +16,23 @@ public class ResponseDAO extends EntityManagerDAO<Response>{
     public Response getByUserAndShiftRequestId(int userId, int requestId) {
         String jpql = "SELECT r FROM Response r WHERE r.user.id = :uid AND r.shiftRequest.id = :rid";
 
-        return em.createQuery(jpql, Response.class)
-                .setParameter("uid", userId)
-                .setParameter("rid", requestId)
-                .getSingleResult();
+        return executeQuery(() ->
+            em.createQuery(jpql, Response.class)
+            .setParameter("uid", userId)
+            .setParameter("rid", requestId)
+            .getSingleResult()
+        );
     }
 
     // ________________________________________________________
 
     public List<Response> getByShiftRequestId(int requestId){
+        String jpql = "SELECT r FROM Response r WHERE r.shiftRequest.id = :id";
 
-        return em.createQuery(
-                "SELECT r FROM Response r WHERE r.shiftRequest.id = :rid",
-                Response.class
-            )
-            .setParameter("rid", requestId)
-            .getResultList();
+        return executeQuery(() ->
+            em.createQuery(jpql, Response.class)
+            .setParameter("id", requestId)
+            .getResultList()
+        );
     }
 }
